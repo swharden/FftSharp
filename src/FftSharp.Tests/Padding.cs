d48a0e56649e5ad2d6827fb6d34b2a8b9e32dde3
Scott W Harden <swharden@gmail.com>
2020-05-26 22:11:02 -0400

    test padding #6

---
+++ src/FftSharp.Tests/Padding.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace FftSharp.Tests
+{
+    class Padding
+    {
+        [Test]
+        public void Test_FFT_AllLengthInput()
+        {
+            for (int i = 1; i < 200; i++)
+            {
+                Complex[] input = new Complex[i];
+                Complex[] padded = FftSharp.Transform.ZeroPad(input);
+                Console.WriteLine($"Length {input.Length} -> {padded.Length}");
+                FftSharp.Transform.FFT(padded);
+            }
+        }
+    }
+}
