b43ea714e867b2cdc8017dffc942a24785bc5f3b
Scott W Harden <swharden@gmail.com>
2020-06-06 13:01:05 -0400

    create ZeroPad() for double arrays #13

--- src/FftSharp/Transform.cs
+++ src/FftSharp/Transform.cs
@@ -16,7 +16,7 @@ namespace FftSharp
         /// <summary>
         /// Return the input array (or a new zero-padded new one) ensuring length is a power of 2
         /// </summary>
-        /// <param name="input">complex array of any length</param>
+        /// <param name="input">array of any length</param>
         /// <returns>the input array or a zero-padded copy</returns>
         public static Complex[] ZeroPad(Complex[] input)
         {
@@ -29,6 +29,27 @@ namespace FftSharp
 
             int difference = targetLength - input.Length;
             Complex[] padded = new Complex[targetLength];
+            Array.Copy(input, 0, padded, difference / 2, input.Length);
+
+            return padded;
+        }
+
+        /// <summary>
+        /// Return the input array (or a new zero-padded new one) ensuring length is a power of 2
+        /// </summary>
+        /// <param name="input">array of any length</param>
+        /// <returns>the input array or a zero-padded copy</returns>
+        public static double[] ZeroPad(double[] input)
+        {
+            if (IsPowerOfTwo(input.Length))
+                return input;
+
+            int targetLength = 1;
+            while (targetLength < input.Length)
+                targetLength *= 2;
+
+            int difference = targetLength - input.Length;
+            double[] padded = new double[targetLength];
             Array.Copy(input, 0, padded, difference / 2, input.Length);
 
             return padded;
