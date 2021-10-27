0d3e807df7653976d5f09fe089ec089c165df60a
Scott W Harden <swharden@gmail.com>
2021-10-27 19:26:14 -0400

    Window: Welch
    #41

---
+++ src/FftSharp/Windows/Welch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FftSharp.Windows
+{
+    internal class Welch : Window, IWindow
+    {
+        public override string Name => "Welch";
+        public override string Description =>
+            "The Welch window is typically used for antialiasing and resampling. " +
+            "Its frequency response is better than that of the Bartlett windowed cosc function below pi, " +
+            "but it shows again a rather distinctive bump above.";
+
+        public override double[] Create(int size, bool normalize = false)
+        {
+            double[] window = new double[size];
+
+            double halfN = (size - 1) / 2.0;
+
+            for (int i = 0; i < size; i++)
+            {
+                double b = (i - halfN) / halfN;
+                window[i] = 1 - b * b;
+            }
+
+            if (normalize)
+                NormalizeInPlace(window);
+
+            return window;
+        }
+    }
+}
