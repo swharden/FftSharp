c317a66c2e8bb6ad9e8f60e58a3db9d211d56e0d
Scott W Harden <swharden@gmail.com>
2022-06-15 23:35:20 -0400

    Test: combine frequency tests

--- src/FftSharp.Tests/FftFreqTests.cs
+++ src/FftSharp.Tests/FftFreqTests.cs
@@ -26,7 +26,7 @@ namespace FftSharp.Tests
 
             // calculate FFT frequencies both ways
             double[] fftFreq = FftSharp.Transform.FFTfreq(sampleRateHz, fft.Length);
-            double[] fftFreq2 = FftSharp.Transform.FFTfreq(sampleRateHz, samples);
+            double[] fftFreq2 = FftSharp.Transform.FFTfreq(sampleRateHz, fft);
             Assert.That(fftFreq2, Is.EqualTo(fftFreq));
 
             ScottPlot.Plot plt1 = new(400, 200);
@@ -37,6 +37,42 @@ namespace FftSharp.Tests
             plt2.AddScatter(fftFreq, fft);
             plt2.AddVerticalLine(2, System.Drawing.Color.Red, style: ScottPlot.LineStyle.Dash);
             TestTools.SaveFig(plt2, "fft");
+        }
+
+        [Test]
+        public void Test_Freq_Lookup()
+        {
+            /* Compare against values generated using Python:
+             * 
+             *   >>> numpy.fft.fftfreq(16, 1/16000)
+             *   array([    0.,  1000.,  2000.,  3000.,  4000.,  5000.,  6000.,  7000.,
+             *          -8000., -7000., -6000., -5000., -4000., -3000., -2000., -1000.])
+             * 
+             */
+
+            int sampleRate = 16000;
+            int pointCount = 16;
+            double[] signal = new double[pointCount];
+            double[] fft = FftSharp.Transform.FFTmagnitude(signal);
+
+            double[] freqsFullKnown = {
+                0,  1000,  2000,  3000,  4000,  5000,  6000,  7000,
+                -8000, -7000, -6000, -5000, -4000, -3000, -2000, -1000
+            };
+            double[] freqsFull = FftSharp.Transform.FFTfreq(sampleRate, signal, oneSided: false);
+            double[] freqsFull2 = FftSharp.Transform.FFTfreq(sampleRate, signal.Length, oneSided: false);
+            Assert.That(freqsFull, Is.EqualTo(freqsFullKnown));
+            Assert.That(freqsFull2, Is.EqualTo(freqsFullKnown));
+
+            double[] freqsHalfKnown = {
+                0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000
+            };
+            double[] freqsHalf = FftSharp.Transform.FFTfreq(sampleRate, fft, oneSided: true);
+            double[] freqsHalf2 = FftSharp.Transform.FFTfreq(sampleRate, fft.Length, oneSided: true);
+            Console.WriteLine(String.Join(",", freqsHalf2.Select(x => x.ToString())));
+            Assert.That(freqsHalf, Is.EqualTo(freqsHalfKnown));
+            Assert.That(freqsHalf2, Is.EqualTo(freqsHalfKnown));
+
         }
     }
 }
