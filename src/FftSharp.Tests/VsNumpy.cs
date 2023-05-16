44af0c94e36f3bf7816fe11b831d0d8de9416637
Scott W Harden <swharden@gmail.com>
2023-05-16 18:22:01 -0400

    Tests: FFT magnitude for small input arrays
    #62

--- src/FftSharp.Tests/VsNumpy.cs
+++ src/FftSharp.Tests/VsNumpy.cs
@@ -87,5 +87,115 @@ namespace FftSharp.Tests
             for (int i = 0; i < fftFreq.Length; i++)
                 Assert.AreEqual(numpyFftFreq[i], fftFreq[i], 1e-10);
         }
+
+        [Test]
+        public void Test_VsNumpy_Fft_Length32()
+        {
+            double[] values = { 0.33, 2.15, 1.44, 1.37, 0.24, 2.6, 3.51, 1.98, 1.88, 0.08,
+                1.82, 1.3, 0.23, -1.16, -1.35, -0.58, -0.84, -1.35, -2.72, -2.53, -0.02,
+                -0.76, -0.48, -2.1, 0.3, 1.86, 1.6, 1.49, 0.58, 2.12, 2.79, 1.99  };
+
+            Assert.AreEqual(32, values.Length);
+
+            double[] numpyFftMag = { 0.5553125, 1.746009233890808, 0.6806147230825694, 0.2700447117556277,
+                0.13510742832008432, 0.19195357237244665, 0.4374273994066255, 0.8583254324650509, 0.2941652649872177,
+                0.12648992889321545, 0.17946592505602227, 0.08145444884938172, 0.1681978420275756, 0.2919426435801102,
+                0.35936842267425245, 0.2704107530566828, 0.05312499999999987 };
+
+            double[] mag = FftSharp.Transform.FFTmagnitude(values);
+
+            Assert.AreEqual(numpyFftMag.Length, mag.Length);
+            for (int i = 0; i < numpyFftMag.Length; i++)
+            {
+                Assert.AreEqual(numpyFftMag[i], mag[i], 1e-10);
+            }
+        }
+
+        [Test]
+        public void Test_VsNumpy_Fft_Length16()
+        {
+            double[] values = { 0.33, 2.15, 1.44, 1.37, 0.24, 2.6, 3.51, 1.98, 1.88, 0.08, 1.82, 1.3, 0.23, -1.16, -1.35, -0.58 };
+
+            Assert.AreEqual(16, values.Length);
+
+            double[] numpyFftMag = { 0.99, 1.3848691625350331, 0.3643928332909465, 1.0042721499156904,
+                0.34613039450473, 0.1401344145671993, 0.2637216582804615, 0.5006617594915167, 0.04499999999999993 };
+
+            double[] mag = FftSharp.Transform.FFTmagnitude(values);
+
+            Assert.AreEqual(numpyFftMag.Length, mag.Length);
+            for (int i = 0; i < numpyFftMag.Length; i++)
+            {
+                Assert.AreEqual(numpyFftMag[i], mag[i], 1e-10);
+            }
+        }
+
+        [Test]
+        public void Test_VsNumpy_Fft_Length8()
+        {
+            double[] values = { 0.33, 2.15, 1.44, 1.37, 0.24, 2.6, 3.51, 1.98 };
+
+            Assert.AreEqual(8, values.Length);
+
+            double[] numpyFftMag = { 1.7025, 0.706710339966861, 1.1495760087962865, 0.33016737480242325, 0.645 };
+
+            double[] mag = FftSharp.Transform.FFTmagnitude(values);
+
+            Assert.AreEqual(numpyFftMag.Length, mag.Length);
+            for (int i = 0; i < numpyFftMag.Length; i++)
+            {
+                Assert.AreEqual(numpyFftMag[i], mag[i], 1e-10);
+            }
+        }
+
+        [Test]
+        public void Test_VsNumpy_Fft_Length4()
+        {
+            double[] values = { 0.33, 2.15, 1.44, 1.37 };
+
+            Assert.AreEqual(4, values.Length);
+
+            double[] numpyFftMag = { 1.3225, 0.6783251432757007, 0.875 };
+
+            double[] mag = FftSharp.Transform.FFTmagnitude(values);
+
+            Assert.AreEqual(numpyFftMag.Length, mag.Length);
+            for (int i = 0; i < numpyFftMag.Length; i++)
+            {
+                Assert.AreEqual(numpyFftMag[i], mag[i], 1e-10);
+            }
+        }
+
+        [Test]
+        public void Test_VsNumpy_Fft_Length2()
+        {
+            double[] values = { 0.33, 2.15 };
+
+            Assert.AreEqual(2, values.Length);
+
+            double[] numpyFftMag = { 1.24, 1.8199999999999998 };
+
+            double[] mag = FftSharp.Transform.FFTmagnitude(values);
+
+            Assert.AreEqual(numpyFftMag.Length, mag.Length);
+            for (int i = 0; i < numpyFftMag.Length; i++)
+            {
+                Assert.AreEqual(numpyFftMag[i], mag[i], 1e-10);
+            }
+        }
+
+        [Test]
+        public void Test_VsNumpy_Fft_Length1()
+        {
+            double[] values = { 0.33 };
+            Assert.Throws<ArgumentException>(() => { FftSharp.Transform.FFTmagnitude(values); });
+        }
+
+        [Test]
+        public void Test_VsNumpy_Fft_Length0()
+        {
+            double[] values = { };
+            Assert.Throws<ArgumentException>(() => { FftSharp.Transform.FFTmagnitude(values); });
+        }
     }
 }
