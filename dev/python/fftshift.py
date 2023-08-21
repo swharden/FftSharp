cdc91052ef5d77e0fdb6b3ed686c9da352520c9f
Scott W Harden <swharden@gmail.com>
2023-08-21 19:51:28 -0400

    tests: demonstrate DFT
    related to #77

--- dev/python/fftshift.py
+++ dev/python/fftshift.py
@@ -10,3 +10,8 @@ if __name__ == "__main__":
     values = np.arange(8)
     print(values)
     print(scipy.fft.fftshift(values))
+
+    values = [ 1, 2, 3, 4, 5, 6, 7]
+    print(values)
+    print(", ".join([f"{x.real:.05f}" for x in scipy.fft.fft(values)]))
+    print(", ".join([f"{x.imag:.05f}" for x in scipy.fft.fft(values)]))
---
+++ src/FftSharp.Tests/ExperimentalTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace FftSharp.Tests;
+
+internal class ExperimentalTests
+{
+    [Test]
+    [System.Obsolete]
+    public void Test_Experimental_DFT()
+    {
+        double[] prime = { 1, 2, 3, 4, 5, 6, 7 };
+        System.Numerics.Complex[] primeComplex = prime.Select(x => new System.Numerics.Complex(x, 0)).ToArray();
+        System.Numerics.Complex[] result = FftSharp.Experimental.DFT(primeComplex);
+
+        // tested with python
+        double[] expectedReal = { 28.00000, -3.50000, -3.50000, -3.50000, -3.50000, -3.50000, -3.50000 };
+        double[] expectedImag = { -0.00000, 7.26782, 2.79116, 0.79885, -0.79885, -2.79116, -7.26782 };
+
+        Assert.AreEqual(expectedReal.Length, result.Length);
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Assert.AreEqual(expectedReal[i], result[i].Real, 1e-5);
+            Assert.AreEqual(expectedImag[i], result[i].Imaginary, 1e-5);
+        }
+    }
+}
